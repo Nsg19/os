116a41dd3a52aabb61013403ee6cebe0ad70f36c
NEELOTPAL SINGH GOUR <37588678+Nsg19@users.noreply.github.com>
2018-03-25 15:01:57 +0530

    Add files via upload

---
+++ GIT HUB 1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int time=0;
+main()
+{
+	int arrival_time[10],burst_time[10],bt[10],i,j,k,n,time_quantum;
+	cout<<"enter no of process\n";
+	cin>>n;
+	cout<<"Enter Time Quantum for all process:\n";
+	cin>>time_quantum;
+	cout<<"================================ARRIVAL TIME==============================================\n";
+	for(i=0;i<n;i++)
+	{
+		cout<<"Arrival time for Process-"<<i+1<<":\n";
+		cin>>arrival_time[i];
+	}
+	cout<<"================================CPU BURST TIME=============================================\n";
+	for(j=0;j<n;j++)
+	{
+		cout<<"CPU burst for Process-"<<j+1<<":\n";
+		cin>>burst_time[j];
+	
+	}
+
+	for(k=0;k<n;k++)
+	{
+		if(burst_time[k]>=time_quantum)
+		{
+
+			burst_time[k]-=time_quantum;
+			time+=time_quantum;
+
+		}
+		else
+		{
+			time=burst_time[k];
+			burst_time[k]=0;
+		}
+		
+	}
+}
